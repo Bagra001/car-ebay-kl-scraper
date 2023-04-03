697a3ee059e23067dbc393b82b0f130281efde54
Bartlomiej Grabelus <Bartek.Grabelus@gmx.net>
2023-04-04 01:28:19 +0200

    use CompletableFuture

--- src/main/kotlin/de/bagra/carwebcrawler/service/CrawlerService.kt
+++ src/main/kotlin/de/bagra/carwebcrawler/service/CrawlerService.kt
@@ -1,25 +1,28 @@
 package de.bagra.carwebcrawler.service
 
+import de.bagra.carwebcrawler.repository.CrawlerDataRepository
 import de.bagra.carwebcrawler.runnable.CrawlerStatistics
 import de.bagra.carwebcrawler.runnable.CrawlerTaskExecutor
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.stereotype.Service
-import org.springframework.util.concurrent.ListenableFuture
+import java.util.concurrent.CancellationException
+import java.util.concurrent.CompletableFuture
 
 
 @Service
-class CrawlerService(val crawlerStatistics: CrawlerStatistics, val taskScheduler: ThreadPoolTaskScheduler) {
+class CrawlerService(val crawlerStatistics: CrawlerStatistics, val taskScheduler: ThreadPoolTaskScheduler, val crawlerDataRepository: CrawlerDataRepository) {
 
 
-    private lateinit var taskState: ListenableFuture<*>
+    private lateinit var taskState: CompletableFuture<*>
 
     fun startCrawling() {
-        taskState = taskScheduler.submitListenable(CrawlerTaskExecutor(crawlerStatistics))
+        taskState = taskScheduler.submitCompletable(CrawlerTaskExecutor(crawlerStatistics, crawlerDataRepository))
     }
 
     fun stopForceCrawling() {
         if (taskState != null && !taskState!!.isDone) {
             crawlerStatistics.interrupt = true
+            taskState.completeExceptionally(throw CancellationException("interrupted manually"))
         }
     }
 
